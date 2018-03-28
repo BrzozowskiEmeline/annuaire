788340cd859bd26749bfde85a5b3ebaa0cb62e93
brzozowski <emeli_ne@hotmail.fr>
2018-03-28 16:43:11 +0200

    28/03/18/17h

--- src/main/java/com/example/demo/controllers/CarnetController.java
+++ src/main/java/com/example/demo/controllers/CarnetController.java
@@ -23,94 +23,105 @@ import com.example.demo.services.CarnetServices;
 
 @RestController
 public class CarnetController {
-//	ArrayList<Carnet> listeCarnets = new ArrayList<Carnet>(Arrays.asList(
-//	new Carnet(1L, Civilite.M, "Besson", "Luc", "1959-12-18", "0000000000", "5 avenue de l'Element", "75015",
-//			"Paris"));
-//	new Carnet((long) 2, Civilite.MME, "Ullmann", "Liv", "1938-12-16", "0101010101", "1-16-5", "22",
-//			"Tokyo"));
-//	new Carnet((long) 3, Civilite.M, "Lynch", "David", "1946-01-20", "0202020202", "423 Fire Terrace",
-//			"59801", "Missoula"));
-			
-//
-//	@GetMapping("/carnets")
-//	public ArrayList<Carnet> getAllRest() {
-//		return this.listeCarnets;
-//	}
-//
-//	@GetMapping("/carnetshtml")
-//	/*
-//	 * public String getAll(Model model) { model.addAttribute("liste",
-//	 * listeCarnets); // ajoute la liste créer en ArrayList return"pages/carnets";
-//	 * // retourne un string, mais on ne veut pas ca
-//	 */
-//
-//	public ModelAndView getAll() { // pour envoyer vers une vue
-//		ModelAndView mav = new ModelAndView("pages/carnets");
-//		mav.addObject("carnets", listeCarnets);
-//		return mav;
-//
-//	}
-////methode qui permet d'envoyer un formulaire
-//	// @GetMapping , GetMapping et Request ici ,c'est exactement la meme chose
-//	@RequestMapping(method = RequestMethod.GET, value = "/carnet/{id}")
-//	public ModelAndView getById(@PathVariable Integer id) {
-//		for (Carnet carnet : listeCarnets) {
-//			if (id.equals(carnet.getId())) {
-//				ModelAndView mav1 = new ModelAndView("pages/carnets");
-//				mav1.addObject("carnets1", carnet);
-//				return mav1;
-//			}
-//		}
-//		return null;
-//
-//	}
-//
-//	@GetMapping("/carnetForm")
-//	public ModelAndView getFormulaire() {
-//		ModelAndView mav2 = new ModelAndView("pages/carnetForm");
-//		return mav2;
-//
-//	}
-//
-//	@RequestMapping(method = RequestMethod.POST, value = "/carnetForm1") // ajout ds le carnet une personne
-//	public ModelAndView ajout(@Validated Carnet carnet, BindingResult bindingResult /*@ModelAttribute Carnet carnet*/) {
-//		if(bindingResult.hasErrors()) {
-//		return new ModelAndView("pages/carnetForm.html").addObject("carnet",carnet);
-//		}
-//	
-//		carnet.setId(listeCarnets.size()+1);
-//		listeCarnets.add(carnet);
-//	return new ModelAndView("pages/carnetFormResult.html").addObject("carnet", carnet);
-//	}
-//
-//	@RequestMapping(value = "/carnet", method = RequestMethod.GET)
-//	public ModelAndView form() {
-//		return new ModelAndView("pages/carnetForm").addObject("carnet", new Carnet());
-//
-//	}
-//
-////	@RequestMapping(value ="/carnet/{id}")
-////	public void delete(@PathVariable Long id) {
-////		Iterator<Carnet> it = listeCarnets.iterator();
-////		while(it.hasNext()) {
-////			Carnet carnet = it.next();
-////			if(carnet.getId().equals(id)) {
-////				it.remove();
-////			}
-////		}
-////	
-////	
-////	this.listeCarnets.removeIf(obj -> obj.getId().equals(id));
-////	}
+	// ArrayList<Carnet> listeCarnets = new ArrayList<Carnet>(Arrays.asList(
+	// new Carnet(1L, Civilite.M, "Besson", "Luc", "1959-12-18", "0000000000", "5
+	// avenue de l'Element", "75015",
+	// "Paris"));
+	// new Carnet((long) 2, Civilite.MME, "Ullmann", "Liv", "1938-12-16",
+	// "0101010101", "1-16-5", "22",
+	// "Tokyo"));
+	// new Carnet((long) 3, Civilite.M, "Lynch", "David", "1946-01-20",
+	// "0202020202", "423 Fire Terrace",
+	// "59801", "Missoula"));
+
+	//
+	// @GetMapping("/carnets")
+	// public ArrayList<Carnet> getAllRest() {
+	// return this.listeCarnets;
+	// }
+	//
+	// @GetMapping("/carnetshtml")
+	// /*
+	// * public String getAll(Model model) { model.addAttribute("liste",
+	// * listeCarnets); // ajoute la liste créer en ArrayList
+	// return"pages/carnets";
+	// * // retourne un string, mais on ne veut pas ca
+	// */
+	//
+	// public ModelAndView getAll() { // pour envoyer vers une vue
+	// ModelAndView mav = new ModelAndView("pages/carnets");
+	// mav.addObject("carnets", listeCarnets);
+	// return mav;
+	//
+	// }
+	//// methode qui permet d'envoyer un formulaire
+	// // @GetMapping , GetMapping et Request ici ,c'est exactement la meme chose
+	// @RequestMapping(method = RequestMethod.GET, value = "/carnet/{id}")
+	// public ModelAndView getById(@PathVariable Integer id) {
+	// for (Carnet carnet : listeCarnets) {
+	// if (id.equals(carnet.getId())) {
+	// ModelAndView mav1 = new ModelAndView("pages/carnets");
+	// mav1.addObject("carnets1", carnet);
+	// return mav1;
+	// }
+	// }
+	// return null;
+	//
+	// }
+	//
+	// @GetMapping("/carnetForm")
+	// public ModelAndView getFormulaire() {
+	// ModelAndView mav2 = new ModelAndView("pages/carnetForm");
+	// return mav2;
+	//
+	// }
+	//
+	// @RequestMapping(method = RequestMethod.POST, value = "/carnetForm1") // ajout
+	// ds le carnet une personne
+	// public ModelAndView ajout(@Validated Carnet carnet, BindingResult
+	// bindingResult /*@ModelAttribute Carnet carnet*/) {
+	// if(bindingResult.hasErrors()) {
+	// return new ModelAndView("pages/carnetForm.html").addObject("carnet",carnet);
+	// }
+	//
+	// carnet.setId(listeCarnets.size()+1);
+	// listeCarnets.add(carnet);
+	// return new ModelAndView("pages/carnetFormResult.html").addObject("carnet",
+	// carnet);
+	// }
+	//
+	// @RequestMapping(value = "/carnet", method = RequestMethod.GET)
+	// public ModelAndView form() {
+	// return new ModelAndView("pages/carnetForm").addObject("carnet", new
+	// Carnet());
+	//
+	// }
+	//
+	//// @RequestMapping(value ="/carnet/{id}")
+	//// public void delete(@PathVariable Long id) {
+	//// Iterator<Carnet> it = listeCarnets.iterator();
+	//// while(it.hasNext()) {
+	//// Carnet carnet = it.next();
+	//// if(carnet.getId().equals(id)) {
+	//// it.remove();
+	//// }
+	//// }
+	////
+	////
+	//// this.listeCarnets.removeIf(obj -> obj.getId().equals(id));
+	//// }
 
 	@Autowired
 	private CarnetServices cs;
 
 	@RequestMapping("/testadd")
-	public void addTest(){
-		cs.add(new Carnet(1L, Civilite.M, "Besson", "Luc", "1959-10-18", "0618555723", "mohamed.chebbi@viacesi.fr", "75015", "Paris"));
-		cs.add(new Carnet(2L, Civilite.MME, "Ullmann", "Liv", "1938-12-16", "061850423", "chebbimohamed1987@live.fr", "59800", "Tokyo"));
-		cs.add(new Carnet(3L, Civilite.M, "Lynch", "David", "1946-01-20", "061850423", "chebbimohamed1987@gmail.com", "59801", "Missoula"));
+	public void addTest() {
+		cs.add(new Carnet(1L, Civilite.M, "Besson", "Luc", "1959-10-18", "0618555723", "mohamed.chebbi@viacesi.fr",
+				"75015", "Paris"));
+		cs.add(new Carnet(2L, Civilite.MME, "Ullmann", "Liv", "1938-12-16", "061850423", "chebbimohamed1987@live.fr",
+				"59800", "Tokyo"));
+		cs.add(new Carnet(3L, Civilite.M, "Lynch", "David", "1946-01-20", "061850423", "chebbimohamed1987@gmail.com",
+				"59801", "Missoula"));
 
 	}
 
@@ -120,23 +131,30 @@ public class CarnetController {
 	}
 
 	// methode renvoie 1 id
-	@RequestMapping(method = RequestMethod.GET, value = "/carnet1/{id}")
+	@RequestMapping(method = RequestMethod.GET, value = "/carnet/{id}")
 	public ModelAndView getById(@PathVariable long id) {
-		ModelAndView mav = new ModelAndView("pages/carnet1");
-		return mav.addObject("carnet",cs.getCarnets(id));
+		ModelAndView mav = new ModelAndView("pages/carnet");
+		return mav.addObject("carnet", cs.getCarnets(id));
 
 	}
 
 	// supprime tout
-//	 @RequestMapping(method= RequestMethod.DELETE, value="/deleteAll")
-//	 public void DeleteAll(@PathVariable Interger id) {
-//	 cs.DeleteAll();
-//	 }
+	// @RequestMapping(method= RequestMethod.DELETE, value="/deleteAll")
+	// public void DeleteAll(@PathVariable Interger id) {
+	// cs.DeleteAll();
+	// }
 
 	// supprime 1 element/id
-	  @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
-	    public ModelAndView delete(@PathVariable Long id){
-	        cs.delete(id);
-	        return new ModelAndView("redirect:/carnets/");
-	    }
+	@RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+	public ModelAndView delete(@PathVariable Long id) {
+		cs.delete(id);
+		return new ModelAndView("redirect:/carnets/");
+	}
+
+	// Modifie 1 id
+	@RequestMapping(method = RequestMethod.PUT, value = "/carnet/{id}")
+	public ModelAndView update(Carnet carnet) {
+		cs.add(carnet);
+		return new ModelAndView("redirect:/carnets/");
+	}
 }
